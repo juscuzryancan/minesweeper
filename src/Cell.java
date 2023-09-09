08188dd39e359829b34ea60b90371ec53fd3be41
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-09-08 20:17:16 -0700

    initial commit

---
+++ src/Cell.java
@@ -0,0 +1,7 @@
+public class Cell {
+    private int x;
+    private int y;
+    private boolean isMine;
+    private boolean isFlagged;
+
+}
---
+++ src/Grid.java
@@ -0,0 +1,5 @@
+public class Grid {
+    private int rows;
+    private int columns;
+    private int numOfMines;
+}
